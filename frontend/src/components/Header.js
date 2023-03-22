667c3016384a6c9c95abbef81a750301205cd1f7
chaofan-w <chaofan.w@gmail.com>
2023-03-21 20:52:04 -0400

    work around of IOS browser bug on menubar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -341,81 +341,97 @@ export default function PrimarySearchAppBar() {
         boxShadow: "none",
         display: "flex",
         justfiyContent: "center",
-        bgcolor: "primary.main",
+        bgcolor: "transparent",
         backdropFilter: "blur(10px)",
       }}
     >
-      <Toolbar sx={{ height: "100%" }}>
-        <IconButton
-          disableRipple
-          onClick={async () => {
-            navigate("/");
-            // const response = await fetchProducts();
-            // dispatch(response);
-          }}
-        >
-          <img
-            src={logoImg}
-            alt="Gadget Go Logo"
-            style={{ width: 35, height: 30 }}
-          />
-          <Typography
-            variant="h6"
-            sx={{
-              ml: 1,
-              color: "white",
-              display: { xs: "none", sm: "block" },
+      <Paper
+        square
+        relvation={0}
+        sx={{
+          width: "100%",
+          maxWidth: "100vw",
+          height: "10vh",
+          boxShadow: "none",
+          backgroundColor: "primary.main",
+        }}
+      >
+        <Toolbar sx={{ height: "100%" }}>
+          <IconButton
+            disableRipple
+            onClick={async () => {
+              navigate("/");
+              // const response = await fetchProducts();
+              // dispatch(response);
             }}
           >
-            Gadget Go!
-          </Typography>
-        </IconButton>
+            <img
+              src={logoImg}
+              alt="Gadget Go Logo"
+              style={{ width: 35, height: 30 }}
+            />
+            <Typography
+              variant="h6"
+              sx={{
+                ml: 1,
+                color: "white",
+                display: { xs: "none", sm: "block" },
+              }}
+            >
+              Gadget Go!
+            </Typography>
+          </IconButton>
 
-        <SearchBar />
-        <Box sx={{ flexGrow: 1 }} />
-        <Box sx={{ display: { xs: "none", md: "flex" } }}>
-          {loginCustomer.length > 0 && (
-            <>
-              <IconButton
-                disabled={carts.length === 0 || carts[0].products.length === 0}
-                size="large"
-                aria-label="show num of items in cart"
-                sx={{ color: "secondary.light" }}
-                onClick={() => navigate("/checkout")}
-              >
-                <Badge badgeContent={getCartAmount()} color="error">
-                  <ShoppingBag />
-                </Badge>
-              </IconButton>
-            </>
-          )}
-          <IconButton
-            size="large"
-            edge="end"
-            aria-label="account of current user"
-            aria-controls={menuId}
-            aria-haspopup="true"
-            onClick={handleProfileMenuOpen}
-            sx={{ color: "secondary.light" }}
-          >
-            {loginCustomer.length > 0 ? <AccountCircle /> : <Login />}
-          </IconButton>
-        </Box>
-        <Box sx={{ display: { xs: "flex", md: "none" } }}>
-          <IconButton
-            size="large"
-            aria-label="show more"
-            aria-controls={mobileMenuId}
-            aria-haspopup="true"
-            onClick={handleMobileMenuOpen}
-            sx={{ color: "secondary.light" }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Box>
-      </Toolbar>
-      {loginCustomer.length > 0 ? renderMobileMenu : renderMobileMenuLoggedOut}
-      {loginCustomer.length > 0 ? renderMenu : renderMenuLoggedOut}
+          <SearchBar />
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {loginCustomer.length > 0 && (
+              <>
+                <IconButton
+                  disabled={
+                    carts.length === 0 || carts[0].products.length === 0
+                  }
+                  size="large"
+                  aria-label="show num of items in cart"
+                  sx={{ color: "secondary.light" }}
+                  onClick={() => navigate("/checkout")}
+                >
+                  <Badge badgeContent={getCartAmount()} color="error">
+                    <ShoppingBag />
+                  </Badge>
+                </IconButton>
+              </>
+            )}
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              sx={{ color: "secondary.light" }}
+            >
+              {loginCustomer.length > 0 ? <AccountCircle /> : <Login />}
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              sx={{ color: "secondary.light" }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+        {loginCustomer.length > 0
+          ? renderMobileMenu
+          : renderMobileMenuLoggedOut}
+        {loginCustomer.length > 0 ? renderMenu : renderMenuLoggedOut}
+      </Paper>
     </AppBar>
     //   {/* </Grid>
     // </Grid> */}
